c9970da370de39f5269f6c5d143a82580740ce17
Orkun Yalınkılıç <orkunyalinkilic69@gmail.com>
2021-03-03 22:02:36 +0300

    Section4_RepositorySiniflarininHazirlanmasi

--- src/app/modell/modell.module.ts
+++ src/app/modell/modell.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RestService } from './rest.service';
+import { ProductRepository } from './product.repository';
+import { CategoryRepository } from './category.repository';
 
-@NgModule({
-    imports: [HttpClientModule]
+@NgModule({  // Modell klasörü altındaki kullanılacak her şey model.module.ts de bu kısımda eklenir. Module mantığı hep böyledir.
+    imports: [HttpClientModule],
+    providers:[RestService,ProductRepository,CategoryRepository]
 })
 
 export class ModelModule { }
---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ModelModule } from '../modell/modell.module';
+
+@NgModule({  
+    imports: [ModelModule],
+    providers:[]
+})
+
+export class ShopModule { }
