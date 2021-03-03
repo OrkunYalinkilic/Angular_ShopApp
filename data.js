4a065112250fd040d2c4ecf844ef41ba0617acba
Orkun Yalınkılıç <orkunyalinkilic69@gmail.com>
2021-03-03 18:29:50 +0300

    Section2_JsonServerYapilandirilmasi

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = function(){
+    return {
+        products:[
+            { id:1,name:"Samsung S5",price:1000,imageUrl:'1.jpg',description:"Lorem Ipsum is simply dummy" },
+            { id:2,name:"Samsung S6",price:2000,imageUrl:'2.jpg',description:"Lorem Ipsum has been the industry's" },
+            { id:3,name:"Samsung S7",price:3000,imageUrl:'3.jpg',description:"Standard dummy text ever" },
+            { id:4,name:"Samsung S8",price:4000,imageUrl:'4.jpg',description:"Also the leap into electronic typesetting" },
+            { id:5,name:"Samsung S9",price:5000,imageUrl:'5.jpg',description:"Letraset sheets containing" },
+            { id:6,name:"Samsung S10",price:6000,imageUrl:'6.jpg',description:"More recently with desktop" },
+            { id:7,name:"Samsung S11",price:7000,imageUrl:'7.jpg',description:"Aldus PageMaker including " }
+
+        ],
+        categories:[
+            {id:1,name:"Telefon"},
+            {id:2,name:"Bilgisayar"},
+            {id:3,name:"Elektronik"}
+        ],
+        orders:[]
+    }
+}
